79e2d686a246a64cf6c1e70fbb4fd41a2e07dc10
subhashgit <72294519+subhashgit@users.noreply.github.com>
2024-06-12 18:30:42 +0530

    latesrt

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCp_rNI-c_PupXnwBQNuKI1yQokNgegOYE",
+    authDomain: "the-shack-57d22.firebaseapp.com",
+    projectId: "the-shack-57d22",
+    storageBucket: "the-shack-57d22.appspot.com",
+    messagingSenderId: "13625150020",
+    appId: "1:13625150020:web:457a07baed980026d28a11",
+    measurementId: "G-RMZZ3FRKZE"
+}
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
